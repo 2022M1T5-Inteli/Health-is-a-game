ee44396b91be709d547aaa9fc9bf9c384649cdf3
gabrio-inteli <99267124+gabrio-inteli@users.noreply.github.com>
2022-03-04 09:42:08 -0300

    chore(game): comment code

--- src/src/FirstClass/BabyBottle.gd
+++ src/src/FirstClass/BabyBottle.gd
@@ -2,8 +2,10 @@ extends Area2D
 
 onready var _babyBottle = $BabyBottleSprite
 
-export var points = 20
+#export var points = 20
 
+# when a body collides with this element, it hides the babyBottle
 func _on_BabyBottle_body_entered(body):
+	# if the body name is "Player", it triggers the hide action mentioned above
 	if body.get_name() == "Player":
 		_babyBottle.hide()
--- src/src/FirstClass/Candy.gd
+++ src/src/FirstClass/Candy.gd
@@ -2,8 +2,10 @@ extends Area2D
 
 onready var _Candy = $CandySprite
 
-export var points = -20
+#export var points = -20
 
+# when a body collides with this element, it hides the candy
 func _on_Candy_body_entered(body):
-		if body.get_name() == "Player":
-			_Candy.hide()
+	# if the body name is "Player", it triggers the hide action mentioned above
+	if body.get_name() == "Player":
+		_Candy.hide()
