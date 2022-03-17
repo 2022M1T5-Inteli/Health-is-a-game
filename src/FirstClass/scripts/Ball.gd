451a96eaa483cd1e15114bad696895f16acafd32
gabrio-inteli <99267124+gabrio-inteli@users.noreply.github.com>
2022-03-17 15:49:09 -0300

    chore(game): insert new implementations

---
+++ src/FirstClass/scripts/Ball.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+onready var _Ball = $BallSprite
+var points = 4
+
+
+func _on_Ball_body_entered(body):
+	if body.get_name() == "PlayerChild":
+		_Ball.hide()
+		if (GameManager.health_score + points) <= 1000:
+			GameManager.health_score = GameManager.health_score + points
---
+++ src/FirstClass/scripts/FriedPotatoes.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+onready var _FriedPotatoes = $FriedPotatoesSprite
+
+var points = -3
+
+
+func _on_FriedPotatoes_body_entered(body):
+	# if the body name is "Player", it triggers the hide action mentioned above
+	if body.get_name() == "PlayerChild":
+		_FriedPotatoes.hide()
+		if (GameManager.health_score + points) <= 1000:
+			GameManager.health_score = GameManager.health_score + points
