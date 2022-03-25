986a7bdf2f0f7bef400288e21179e6f2c615cba6
gabrio-inteli <99267124+gabrio-inteli@users.noreply.github.com>
2022-03-25 10:26:40 -0300

    fix(game): fix camera proportion

--- src/FirstClass/scripts/Camera2D.gd
+++ src/FirstClass/scripts/Camera2D.gd
@@ -1,11 +1,7 @@
 extends Camera2D
 
 func _ready():
-	var windowSize = OS.get_screen_size()
-	var proportion = windowSize[0]/windowSize[1]
-	zoom.x = zoom.x / proportion
-	zoom.y = zoom.y / proportion
-
+	pass
 
 func _process(delta):
 	pass
