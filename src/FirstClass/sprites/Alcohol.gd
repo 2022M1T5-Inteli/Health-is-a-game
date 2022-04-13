bed455ead514afffd888a50000ee169e4f3e27f7
gabriosilva <gabrio.silva@sou.inteli.edu.br>
2022-04-12 23:39:34 -0300

    refact(game): change healthbar scale and implement ending animations

--- src/FirstClass/sprites/Alcohol.gd
+++ src/FirstClass/sprites/Alcohol.gd
@@ -9,6 +9,7 @@ func _on_Alcohol_body_entered(body):
 	# if the body name is "Player", it triggers the hide action mentioned above
 	if body.get_name() == "Player":
 		$Alcohol.hide()
+		body.play_bad_hit()
 		if (GameManager.health_score + points) <= 1000:
 			GameManager.health_score = GameManager.health_score + points
 	$CollectedSoundBad.play()
